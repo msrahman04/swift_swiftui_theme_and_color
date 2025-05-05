5c41299428e7f5047aa4a4e7d486dcdbf267a758
Shahin <msrahman.mail@gmail.com>
2025-05-05 08:22:47 +0600

    Initial Commit

---
+++ SwiftSwiftUIThemeAndColour/SwiftSwiftUIThemeAndColourApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSwiftUIThemeAndColourApp.swift
+//  SwiftSwiftUIThemeAndColour
+//
+//  Created by Shahinur Rahman on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSwiftUIThemeAndColourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftSwiftUIThemeAndColourTests/SwiftSwiftUIThemeAndColourTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSwiftUIThemeAndColourTests.swift
+//  SwiftSwiftUIThemeAndColourTests
+//
+//  Created by Shahinur Rahman on 5/5/25.
+//
+
+import Testing
+@testable import SwiftSwiftUIThemeAndColour
+
+struct SwiftSwiftUIThemeAndColourTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
